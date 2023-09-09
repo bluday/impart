7129995a4ce1f921b35177a658806b6749ad0f0f
BluDay <19403893+BluDay@users.noreply.github.com>
2023-09-09 23:28:06 +0200

    Field syntax update in BluConstants.

--- src/BluDay.Common/BluConstants.cs
+++ src/BluDay.Common/BluConstants.cs
@@ -2,43 +2,70 @@
 {
     public static class BluConstants
     {
-        public const char
-            CLOSING_PARENTHESIS_CHAR = ')',
-            COMMA_CHAR               = ',',
-            OPENING_PARENTHESIS_CHAR = '(',
-            NEW_LINE_CHAR            = '\n',
-            PERIOD_CHAR              = '.',
-            RIGHT_ARROW_CHAR         = '>',
-            TAB_CHAR                 = '\t';
+        #region Char
+        public const char CLOSING_PARENTHESIS_CHAR = ')';
 
-        public const int
-            DEFAULT_NOTIFICATION_DURATION = 10 * 1000;
+        public const char COMMA_CHAR = ',';
 
-        public const string
-            // Don't f#€k with this. — Joe "I did DMT 3 days in a row" Rogan (Probably)
-            ASCII_ELLIPSES              = "...",
-            APP_RESOURCE_PATH_PREFIX    = "ms-appx:///",
-            DEFAULT_CONFIG_PATH         = "config.json",
-            DEFAULT_LOGGER_FORMAT       = "{DateTime:yyyy-MM-dd HH:mm:ss.fff} [{Level,-7}] {Message}{NewLine}{Content}",
-            FILE_EXTENSION_JSON         = ".json",
-            FILE_EXTENSION_YAML         = ".yml",
-            LOGGER_FORMAT_REGEX_PATTERN = @"\{([a-zA-Z0-9]+)((:([a-zA-Z0-9-._\s:]+)))?\}*",
-            PRINTABLE_LIST_OUTER_FORMAT = "{{\n{0}\n}}";
+        public const char OPENING_PARENTHESIS_CHAR = '(';
 
-        public const string
-            // Should I even do this?
-            Contextual = "Contextual",
-            DateTime   = "DateTime",
-            Default    = "Default",
-            Event      = "Event",
-            Level      = "Level",
-            Model      = "Model",
-            Name       = "Name",
-            NaN        = "NaN",
-            None       = "None",
-            Text       = "Text",
-            Untitled   = "Untitled",
-            View       = "View",
-            ViewModel  = "ViewModel";
+        public const char NEW_LINE_CHAR = '\n';
+
+        public const char PERIOD_CHAR = '.';
+
+        public const char RIGHT_ARROW_CHAR = '>';
+
+        public const char TAB_CHAR = '\t';
+        #endregion
+
+        #region Int32
+        public const int DEFAULT_NOTIFICATION_DURATION = 10 * 1000;
+        #endregion
+
+        #region String
+        // Don't f#€k with this. — Joe "I did DMT 3 days in a row" Rogan (Probably)
+        public const string ASCII_ELLIPSES = "...";
+
+        public const string APP_RESOURCE_PATH_PREFIX = "ms-appx:///";
+
+        public const string DEFAULT_CONFIG_PATH = "config.json";
+
+        public const string DEFAULT_LOGGER_FORMAT = "{DateTime:yyyy-MM-dd HH:mm:ss.fff} [{Level,-7}] {Message}{NewLine}{Content}";
+
+        public const string FILE_EXTENSION_JSON = ".json";
+
+        public const string FILE_EXTENSION_YAML = ".yml";
+
+        public const string LOGGER_FORMAT_REGEX_PATTERN = @"\{([a-zA-Z0-9]+)((:([a-zA-Z0-9-._\s:]+)))?\}*";
+
+        public const string PRINTABLE_LIST_OUTER_FORMAT = "{{\n{0}\n}}";
+
+        // Should I even do this?
+        public const string Contextual = "Contextual";
+
+        public const string DateTime = "DateTime";
+
+        public const string Default = "Default";
+
+        public const string Event = "Event";
+
+        public const string Level = "Level";
+
+        public const string Model = "Model";
+
+        public const string Name = "Name";
+
+        public const string NaN = "NaN";
+
+        public const string None = "None";
+
+        public const string Text = "Text";
+
+        public const string Untitled = "Untitled";
+
+        public const string View = "View";
+
+        public const string ViewModel = "ViewModel";
+        #endregion
     }
 }
