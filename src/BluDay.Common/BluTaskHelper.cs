2b64fed47d9015ca37304785810bc7d713b12ee0
BluDay <19403893+BluDay@users.noreply.github.com>
2023-10-13 16:04:02 +0200

    refactor: re-write the BluTaskHelper class

--- src/BluDay.Common/BluTaskHelper.cs
+++ src/BluDay.Common/BluTaskHelper.cs
@@ -1,49 +1,17 @@
-using System;
-using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
-using Windows.System;
 
 namespace BluDay.Common
 {
     public static class BluTaskHelper
     {
-        public static DispatcherQueue DispatcherQueue { get; }
-
-        public static DispatcherQueuePriority DefaultPriority { get; set; }
-
-        static BluTaskHelper()
+        public static Windows.UI.Core.CoreDispatcher MainDispatcher
         {
-            DispatcherQueue = CoreApplication.MainView.DispatcherQueue;
-
-            DefaultPriority = DispatcherQueuePriority.High;
+            get => CoreApplication.MainView.Dispatcher;
         }
 
-        public static Task EnqueueDelegate(Action function)
+        public static Windows.System.DispatcherQueue MainDispatcherQueue
         {
-            var task = new Task(function);
-
-            DispatcherQueue.TryEnqueue(async () => await task);
-
-            return task;
-        }
-
-        public static Task<TResult> EnqueueDelegate<TResult>(Func<TResult> function)
-        {
-            var completionSource = new TaskCompletionSource<TResult>();
-
-            DispatcherQueue.TryEnqueue(() =>
-            {
-                try
-                {
-                    completionSource.SetResult(function());
-                }
-                catch (Exception ex)
-                {
-                    completionSource.SetException(ex);
-                }
-            });
-
-            return completionSource.Task;
+            get => CoreApplication.MainView.DispatcherQueue;
         }
     }
 }
