39f1907d2d8cfbac95861d1b9496635f6f856c48
BluDay <19403893+BluDay@users.noreply.github.com>
2023-11-14 18:29:50 +0100

    feat: add BluEventService files

---
+++ src/BluDay.Common/Services/IBluEventService.cs
@@ -0,0 +1,35 @@
+﻿using BluDay.Common.Events;
+using BluDay.Common.Messaging;
+using System.Threading.Tasks;
+
+namespace BluDay.Common.Services
+{
+    public interface IBluEventService
+    {
+        int TopicsCount { get; }
+
+        BluEventTopicInfo[] TopicInfos { get; }
+
+        BluEventTopicInfo GetTopicInfoByPropertyValue(object value);
+
+        Task<BluEventInfo<TEvent>> NotifyAsync<TEvent>(object sender, TEvent e)
+            where TEvent : IBluEvent;
+
+        Task<BluEventInfo<TEvent>> NotifyAsync<TEvent>(string topicName, object sender, TEvent e)
+            where TEvent : IBluEvent;
+
+        Task<bool> SubscribeAsync<TEvent>(BluEventHandler<TEvent> handler)
+            where TEvent : IBluEvent;
+
+        Task<bool> SubscribeAsync<TEvent>(string topicName, BluEventHandler<TEvent> handler)
+            where TEvent : IBluEvent;
+
+        Task<bool> UnsubscribeAsync<TEvent>(BluEventHandler<TEvent> handler)
+            where TEvent : IBluEvent;
+
+        Task<bool> UnsubscribeAsync<TEvent>(string topicName, BluEventHandler<TEvent> handler)
+            where TEvent : IBluEvent;
+
+        Task<BluEventTopicInfo[]> UnsubscribeAsync(object target);
+    }
+}
