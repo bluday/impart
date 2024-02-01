9e05af0dc05024aa80a9b0e89b91b7000a243ed4
BluDay <19403893+BluDay@users.noreply.github.com>
2024-02-01 15:34:59 +0100

    feat: add untracked files

---
+++ src/BluDay.Impart.App/UI/IThemeService.cs
@@ -0,0 +1,6 @@
+namespace BluDay.Impart.App.UI;
+
+public interface IThemeService
+{
+    AppTheme CurrentTheme { get; set; }
+}
---
+++ src/BluDay.Impart.App/UI/ThemeService.cs
@@ -0,0 +1,6 @@
+namespace BluDay.Impart.App.UI;
+
+public sealed class ThemeService : IThemeService
+{
+    public AppTheme CurrentTheme { get; set; }
+}
