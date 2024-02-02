8a59290d6ac46b34a056257b08833b9e9b2abd17
BluDay <19403893+BluDay@users.noreply.github.com>
2024-02-02 14:15:29 +0100

    chore: remove symmetric syntax formatting for now

--- src/BluDay.Impart.App/ImpartAppContainer.cs
+++ src/BluDay.Impart.App/ImpartAppContainer.cs
@@ -28,10 +28,10 @@ internal sealed class ImpartAppContainer : IDisposable
     {
         return new ServiceCollection()
             .AddSingleton<IMessenger>(WeakReferenceMessenger.Default)
-            .AddSingleton<IAppDialogService,     AppDialogService>()
+            .AddSingleton<IAppDialogService, AppDialogService>()
             .AddSingleton<IAppNavigationService, AppNavigationService>()
-            .AddSingleton<IAppThemeService,      AppThemeService>()
-            .AddSingleton<IAppWindowService,     AppWindowService>()
+            .AddSingleton<IAppThemeService, AppThemeService>()
+            .AddSingleton<IAppWindowService, AppWindowService>()
             .AddSingleton<IViewModelProvider, ViewModelProvider>();
     }
 
