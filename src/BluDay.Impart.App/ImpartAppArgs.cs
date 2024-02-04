6fbc729fab151fb10e8cf8115daa9ad7115f1bfe
BluDay <19403893+BluDay@users.noreply.github.com>
2024-02-04 21:27:09 +0100

    feat: add new --skip_intro argument

--- src/BluDay.Impart.App/ImpartAppArgs.cs
+++ src/BluDay.Impart.App/ImpartAppArgs.cs
@@ -3,10 +3,13 @@ namespace BluDay.Impart.App;
 public sealed class ImpartAppArgs
 {
     [CommandLineArgument(["-t", "--theme"], actionType: ArgumentActionType.ParseValue)]
-    public AppTheme? AppTheme { get; init; }
+    public AppTheme AppTheme { get; init; }
 
     [CommandLineArgument(["-d", "--demo"])]
-    public bool? DemoMode { get; init; }
+    public bool DemoMode { get; init; }
+
+    [CommandLineArgument(["--skip_intro"])]
+    public bool SkipIntro { get; init; }
 
     [CommandLineArgument(["-v", "--verbose"], constant: 1)]
     public int Verbosity { get; init; }
