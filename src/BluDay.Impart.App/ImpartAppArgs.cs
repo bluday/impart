d475ea20d9bd519b3c3ad8f6af9555b568a827de
BluDay <19403893+BluDay@users.noreply.github.com>
2024-02-07 15:23:16 +0100

    refactor: change ImpartAppArgs.AppTheme type from AppTheme enum to nullable string

--- src/BluDay.Impart.App/ImpartAppArgs.cs
+++ src/BluDay.Impart.App/ImpartAppArgs.cs
@@ -2,9 +2,6 @@ namespace BluDay.Impart.App;
 
 public sealed class ImpartAppArgs
 {
-    [CommandLineArg(["-t", "--theme"], ActionType = ArgActionType.ParseValue)]
-    public AppTheme AppTheme { get; init; }
-
     [CommandLineArg(["-d", "--demo"])]
     public bool DemoMode { get; init; }
 
@@ -13,4 +10,7 @@ public sealed class ImpartAppArgs
 
     [CommandLineArg(["-v", "--verbose"], Constant = 1)]
     public int Verbosity { get; init; }
+
+    [CommandLineArg(["-t", "--theme"], ActionType = ArgActionType.ParseValue)]
+    public string? AppTheme { get; init; }
 }
