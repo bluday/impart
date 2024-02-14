47c38253071c62e5f2f543d5cf870e721b52a278
BluDay <19403893+BluDay@users.noreply.github.com>
2024-02-14 23:59:29 +0100

    chore: remove init accessors for now

--- src/BluDay.Impart.App/ImpartAppArgs.cs
+++ src/BluDay.Impart.App/ImpartAppArgs.cs
@@ -3,14 +3,14 @@ namespace BluDay.Impart.App;
 public sealed class ImpartAppArgs
 {
     [CommandLineArg(["-d", "--demo"])]
-    public bool DemoMode { get; init; }
+    public bool DemoMode { get; }
 
     [CommandLineArg(["--skip_intro"])]
-    public bool SkipIntro { get; init; }
+    public bool SkipIntro { get; }
 
     [CommandLineArg(["-v", "--verbose"], constant: 1)]
-    public int Verbosity { get; init; }
+    public int Verbosity { get; }
 
     [CommandLineArg(["-t", "--theme"], actionType: ArgActionType.ParseValue)]
-    public string? AppTheme { get; init; }
+    public string? AppTheme { get; }
 }
